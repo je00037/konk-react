06f4ebd81179e36d7c9c11ab47533e8c606991b6
Joe Ellis <joe.p.ellis@googlemail.com>
2021-06-15 17:16:59 +0100

    fixed click handler logic to get new quote

--- src/components/QuoteContainer.js
+++ src/components/QuoteContainer.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import StartButton from './StartButton.js';
 import ItsKevButton from './ItsKevButton.js';
 import NotKevButton from './NotKevButton.js';
+
 import '../css/App.css';
 
 const QuoteContainer = () => {
-  const [quote, setQuote] = useState("Press the button for a quote...");  
+  const [currentQuote, setQuote] = useState("Press the button for a quote...");
+
   return (
         <div className="Quote-container">
             <div className="Quote-text-container">
-                <p className="Quote-text">{quote}</p>
+                <p className="Quote-text">{currentQuote}</p>
             </div>
-            <StartButton currentQuote={quote} changeTheQuote={setQuote}/>
+            <StartButton currentQuoteProp={currentQuote} setQuoteProp={setQuote} />
             <ItsKevButton />
             <NotKevButton />
         </div>
--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,28 +1,23 @@
-import newQuote from '../utils/quoteGenerator';
 import '../css/App.css';
+import newQuote from '../utils/quoteGenerator';
 
 const StartButton = (props) => {
 
-    let currentQuote = props.currentQuote;
-    let nextQuote;
-    let regenCounter = 0;
-
-    do {
-        nextQuote = newQuote();
-        regenCounter++;
-        console.log(regenCounter);
-    } while (nextQuote === currentQuote); 
+    const handleClick = (event) => {
+        console.log(event);
+        let quote;
+        do {
+            quote = newQuote();
+        } while (props.currentQuoteProp === quote); 
+        props.setQuoteProp(quote);
+    }
 
     return (
         <div>
-            <button className="Button Button-orange" onClick={() => {props.changeTheQuote(nextQuote)}}>Let's go!</button>
+            <button className="Button Button-orange" onClick={handleClick}>Let's go!</button>
         </div>
     )
 }
 
 export default StartButton;
 
-/* why do I need to pass a function which accesses the prop to the click handler, instead of just passing the prop itself?
-and can i declare that function somewhere else in either the component or file so i can just pass the function name instead of 
-the full function?
-*/
