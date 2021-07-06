971317263d7d03d8654d880e4dfd113911d48299
Joe Ellis <joe.p.ellis@googlemail.com>
2021-07-06 16:16:27 +0100

    initial attempt to fix massive kev

--- src/components/KevContainer.js
+++ src/components/KevContainer.js
@@ -3,9 +3,7 @@ import '../css/App.css';
 
 const KevContainer = () => {
     return (
-        <div>
             <img src={kev} className="Kev-image" alt="kevin-mccloud" />
-        </div>
     );
 }
 
