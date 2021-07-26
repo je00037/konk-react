967b77e19e976502f02449e39a31c795519a23a0
Joe Ellis <joe.p.ellis@googlemail.com>
2021-07-26 14:48:02 +0100

    added leaderboard modal

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -78,7 +78,7 @@ const AppContent = () => {
 
   return (
     <>
-      {scoresModalShow ? <LeaderboardModal handleClose={handleScoresModal} /> : null}
+      {scoresModalShow ? <LeaderboardModal handleClose={handleScoresModal} points={points} status={status} /> : null}
       <div className="Points-row">
         <Points pointsProp={points} questionsAskedProp={questionsAsked} />
         <ResetButton clickHandler={handleResetClick} />
--- src/components/LeaderboardModal.js
+++ src/components/LeaderboardModal.js
@@ -1,10 +1,13 @@
 import '../css/App.css';
+import { useState } from 'react';
 
 const LeaderboardModal = ({handleClose}) => {
+
     return (
         <div className="modal display-block">
             <section className="modal-main">
-                <button type="button" onClick={handleClose}>Close</button>
+                <h3>Here are the current high scores:</h3>
+                <button type="button" className="Button Button-grey" onClick={handleClose}>Close</button>
             </section>
         </div>
     )
