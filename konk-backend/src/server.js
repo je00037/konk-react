aa1f4bf0ff5dcef8e86428866d0b725738608026
Joe Ellis <joe.ellis@and.digital>
2024-12-02 21:28:44 +0000

    KONKING

--- konk-backend/src/server.js
+++ konk-backend/src/server.js
@@ -1,48 +1,52 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import path from 'path';
+const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.static(path.join(__dirname, "/build")));
 app.use(bodyParser.json());
 
-app.get('/leaderboard', async (req, res) => {
-    try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('konk-data');
-
-        const leaderboardData = await db.collection('leaderboard').find({}).sort({"points": -1}).limit(10).toArray();
-        res.set('Access-Control-Allow-Origin', '*');
-        res.status(200).json(leaderboardData);
-        client.close();
-    } catch (error) {
-        res.status(500).json({ message: "error!", error });
-    }
+app.get("/leaderboard", async (req, res) => {
+  try {
+    const client = new MongoClient("mongodb://localhost:27017");
+    const db = client.db("konk-data");
+    const leaderboardData = await db
+      .collection("leaderboard")
+      .find({})
+      .sort({ points: -1 })
+      .limit(10)
+      .toArray();
+    res.set("Access-Control-Allow-Origin", "*");
+    res.status(200).json(leaderboardData);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "error!", error });
+  }
 });
 
-app.post('/leaderboard', async (req, res) => {    
-    try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('konk-data');
-        const { user, points } = req.body;
+app.post("/leaderboard", async (req, res) => {
+  try {
+    const client = new MongoClient("mongodb://localhost:27017");
+    const db = client.db("konk-data");
+    const { user, points } = req.body;
 
-        await db.collection('leaderboard').insertOne(
-            {
-                "user": user,
-                "points": points,
-            }
-        );
-        res.status(200).json({ message: "success!" });
-        client.close();
-    } catch (error) {
-        res.status(500).json({ message: "error!", error})
-    }
+    await db.collection("leaderboard").insertOne({
+      user: user,
+      points: points,
+    });
+    res.status(200).json({ message: "success!" });
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "error!", error });
+  }
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 
 app.listen(8000, () => console.log("listening on port 8000"));
--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,24 +1,27 @@
-import '../css/App.css';
-import React, { useState, useEffect } from 'react';
-import KevContainer from './KevContainer.js';
-import QuoteContainer from './QuoteContainer.js';
-import StartButton from './StartButton.js';
-import ItsKevButton from './ItsKevButton.js';
-import NotKevButton from './NotKevButton.js';
-import Points from './Points.js';
-import ResetButton from './ResetButton.js';
-import LeaderboardButton from './LeaderboardButton.js';
-import LeaderboardModal from './LeaderboardModal.js';
-import SubmitScore from './SubmitScore.js';
-import { newQuote, quotes } from '../utils/quoteGenerator';
+import "../css/App.css";
+import React, { useState, useEffect } from "react";
+import KevContainer from "./KevContainer.js";
+import QuoteContainer from "./QuoteContainer.js";
+import StartButton from "./StartButton.js";
+import ItsKevButton from "./ItsKevButton.js";
+import NotKevButton from "./NotKevButton.js";
+import Points from "./Points.js";
+import ResetButton from "./ResetButton.js";
+import LeaderboardButton from "./LeaderboardButton.js";
+import LeaderboardModal from "./LeaderboardModal.js";
+import SubmitScore from "./SubmitScore.js";
+import { newQuote, quotes } from "../utils/quoteGenerator";
 
-const placeholderQuote = {author: "Placeholder", quotation: "Hit the button to start."};
+const placeholderQuote = {
+  author: "Placeholder",
+  quotation: "Hit the button to start.",
+};
 let usedQuotes = [];
 let questionsAsked = 0;
 let leaderboardData;
 
 const fetchData = async () => {
-  const result = await fetch('/leaderboard');
+  const result = await fetch("http://localhost:8000/leaderboard");
   leaderboardData = await result.json();
 };
 
@@ -42,74 +45,87 @@ const AppContent = () => {
   const handleStartClick = () => {
     if (usedQuotes.length === quotes.length - 1) {
       setStatus("end");
-      } else {
-          let newQuoteObject;
-          do {
-            newQuoteObject = newQuote();
-          } while (usedQuotes.includes(newQuoteObject.id));
-          usedQuotes.push(newQuoteObject.id);
-          setQuote(newQuoteObject);
-          questionsAsked += 1;
-          setStatus("open");
-        }
-  }
+    } else {
+      let newQuoteObject;
+      do {
+        newQuoteObject = newQuote();
+      } while (usedQuotes.includes(newQuoteObject.id));
+      usedQuotes.push(newQuoteObject.id);
+      setQuote(newQuoteObject);
+      questionsAsked += 1;
+      setStatus("open");
+    }
+  };
 
   const handleResetClick = () => {
     questionsAsked = 0;
     setStatus("reset");
-  }
+  };
 
-  const handleGuessClick = (buttonPressed) =>  { 
-    
+  const handleGuessClick = (buttonPressed) => {
     if (buttonPressed === "kev") {
-      if (quote.author === "Kevin McCloud") { 
+      if (quote.author === "Kevin McCloud") {
         setStatus("correct-kev");
-        setPoints(points => {
-            return points + 1;
+        setPoints((points) => {
+          return points + 1;
         });
-  } else {
+      } else {
         setStatus("incorrect-kev");
       }
-    } 
+    }
 
     if (buttonPressed === "not-kev") {
-      if (quote.author !== "Kevin McCloud") { 
+      if (quote.author !== "Kevin McCloud") {
         setStatus("correct-notkev");
-        setPoints(points => {
-            return points + 1
+        setPoints((points) => {
+          return points + 1;
         });
-    } else {
-          setStatus("incorrect-notkev");}
-    }  
-  }
+      } else {
+        setStatus("incorrect-notkev");
+      }
+    }
+  };
 
   const handleScoresModal = async () => {
     await fetchData();
     setScoresModalShow(!scoresModalShow);
-  }
+  };
 
   return (
     <>
-      {scoresModalShow ? <LeaderboardModal setDisplay={handleScoresModal} apiData={leaderboardData} /> : null}
+      {scoresModalShow ? (
+        <LeaderboardModal
+          setDisplay={handleScoresModal}
+          apiData={leaderboardData}
+        />
+      ) : null}
       <div className="Points-row">
         <Points pointsProp={points} questionsAskedProp={questionsAsked} />
         <ResetButton clickHandler={handleResetClick} />
         <LeaderboardButton showHide={handleScoresModal} />
       </div>
-      <div className='App-content'>
-        <div className='Kev-container'>
+      <div className="App-content">
+        <div className="Kev-container">
           <KevContainer />
         </div>
-      <div className="Quote-container">
-        <QuoteContainer statusProp={status} quoteProp={quote} pointsProp={points} />
-        {status === "end" ? <SubmitScore points={points} /> : null}
-        <StartButton statusProp={status} clickHandler={handleStartClick} resetClickHandler={handleResetClick} />
-        <ItsKevButton statusProp={status} clickHandler={handleGuessClick} />
-        <NotKevButton statusProp={status} clickHandler={handleGuessClick} />
+        <div className="Quote-container">
+          <QuoteContainer
+            statusProp={status}
+            quoteProp={quote}
+            pointsProp={points}
+          />
+          {status === "end" ? <SubmitScore points={points} /> : null}
+          <StartButton
+            statusProp={status}
+            clickHandler={handleStartClick}
+            resetClickHandler={handleResetClick}
+          />
+          <ItsKevButton statusProp={status} clickHandler={handleGuessClick} />
+          <NotKevButton statusProp={status} clickHandler={handleGuessClick} />
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
-}
+};
 
 export default AppContent;
